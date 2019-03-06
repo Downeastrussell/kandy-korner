d5a3302e4ba414723cee5c30d9fac4b823c4cf88
Russell Miller <bet2buy@gmail.com>
2019-03-06 14:01:27 -0500

    kandy korner with API fetch

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -1,53 +1,46 @@
 import { Route } from 'react-router-dom'
 import React, { Component } from "react"
 import CandyList from './components/candylist'
-import LocationList from './components/stores'
+import StoresList from './components/stores'
 import EmployeeList from './components/employees'
 
 
 
 class ApplicationViews extends Component {
-    /*
-        Although you will eventually be pulling your objects
-        from your json-server API, for this chapter, we're
-        faking it and just creating those arrays in the component
-        itself
-    */
-   employeesFromAPI = [
-    { id: 1, name: "Jessica Younker" },
-    { id: 2, name: "Jordan Nelson" },
-    { id: 3, name: "Zoe LeBlanc" },
-    { id: 4, name: "Blaise Roberts" }
-]
-
-// This will eventually get pulled from the API
-storesAPI = [
-    { id: 1, name: "Nashville North", address: "500 Circle Way" },
-    { id: 2, name: "Nashville South", address: "10101 Binary Court" }
-]
-
-candyAPI = [
-    {id:1, name: "Jolly Ranchers", typeId: "2"},
-    {id:2, name: "Snikers", typeId: "1"},
-    {id:3, name: "Twix", typeId: "3"},
-    {id:4, name: "Receese", typeId: "4"},
-
-]
-
-
-
-
-state = {
-    employees: this.employeesFromAPI,
-    stores: this.storesAPI,
-    candy: this.candyAPI,
+   state = {
+    employees: [],
+    stores: [],
+    candy: []
 
 }
+
+componentDidMount() {
+    const newState = {}
+
+    fetch("http://localhost:5002/store")
+        .then(r => r.json())
+        .then(store => {newState.stores = store})
+        .then(() => fetch("http://localhost:5002/employees")
+        .then(r => r.json()))
+        .then(employees => newState.employees = employees)
+        .then(() => fetch("http://localhost:5002/candy")
+        .then((r)=>r.json()))
+        .then(candy => {newState.candy = candy
+
+            this.setState(newState)})
+
+}
+
+
+
+
+
+
     render() {
         return (
             <div className="container-div">
                 <Route exact path="/" render={(props) => {
-                    return <LocationList stores={this.state.stores} />
+                    return <StoresList stores={this.state.stores} />
                 }} />
                 <Route path="/candy" render={(props) => {
                     return <CandyList candy={this.state.candy} />
